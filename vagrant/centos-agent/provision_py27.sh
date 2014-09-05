3e38105b455e2e13c8a774ecfab75b6583da212b
nir0s <nir36g@gmail.com>
2014-09-05 19:08:12 +0300

    fixed py27 provisioning

--- vagrant/centos-agent/provision_py27.sh
+++ vagrant/centos-agent/provision_py27.sh
@@ -35,12 +35,12 @@ sudo /usr/bin/python2.7 get-pip.py &&
 
 # install packman
 #sudo /usr/bin/pip2.7 install https://github.com/cloudify-cosmo/packman/archive/develop.tar.gz
-git clone https://github.com/cloudify-cosmo/packman.git
+sudo git clone https://github.com/cloudify-cosmo/packman.git
 pushd packman
 	if [ -n "$PACKMAN_SHA" ]; then
 		git reset --hard $PACKMAN_SHA
 	fi
-	pip install .
+	sudo pip install .
 popd
 
 
@@ -53,44 +53,34 @@ echo '# create package resources'
 sudo pkm get -c centos-agent
 
 echo '# GET PROCESS'
-/centos-agent/env/bin/pip install celery==3.0.24
-git clone https://github.com/cloudify-cosmo/cloudify-rest-client.git
+sudo /centos-agent/env/bin/pip install celery==3.0.24
+sudo git clone https://github.com/cloudify-cosmo/cloudify-rest-client.git
 pushd cloudify-rest-client
 	if [ -n "$REST_CLIENT_SHA" ]; then
 		git reset --hard $REST_CLIENT_SHA
 	fi
-	/centos-agent/env/bin/pip install .
+	sudo /centos-agent/env/bin/pip install .
 popd
-git clone https://github.com/cloudify-cosmo/cloudify-plugins-common.git
+sudo sgit clone https://github.com/cloudify-cosmo/cloudify-plugins-common.git
 pushd cloudify-plugins-common
 	if [ -n "$COMMON_PLUGIN_SHA" ]; then
 		git reset --hard $COMMON_PLUGIN_SHA
 	fi
-	/centos-agent/env/bin/pip install .
+	sudo /centos-agent/env/bin/pip install .
 popd
-git clone https://github.com/cloudify-cosmo/cloudify-manager.git
+sudo git clone https://github.com/cloudify-cosmo/cloudify-manager.git
 pushd cloudify-manager
 	if [ -n "$MANAGER_SHA" ]; then
 		git reset --hard $MANAGER_SHA
 	fi
 	pushd plugins/plugin-installer
-	  /centos-agent/env/bin/pip install .
-	popd
-	pushd plugins/agent-installer
-	  /centos-agent/env/bin/pip install .
-	popd
-	pushd plugins/windows-agent-installer
-	  /centos-agent/env/bin/pip install .
-	popd
-	pushd plugins/windows-plugin-installer
-	  /centos-agent/env/bin/pip install .
+	  sudo /centos-agent/env/bin/pip install .
 	popd
 popd
-
 
 # create package
 sudo pkm pack -c centos-agent
 sudo pkm pack -c cloudify-centos-agent
 
 echo bootstrap done
-echo NOTE: currently, using some of the packman's features requires that it's run as sudo.
+echo NOTE: currently, using some of packman's features requires that it's run as sudo.
